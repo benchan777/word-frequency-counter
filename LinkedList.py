3a569ff6e2137c5741f02a9b9fe58aa2a3132b21
benchan777 <benchan777@gmail.com>
2021-02-19 15:40:36 -0800

    added function to remove duplicate

--- LinkedList.py
+++ LinkedList.py
@@ -11,7 +11,7 @@ class LinkedList:
     new_node.next = self.head
     self.head = new_node
     return new_node
-    
+
 
   def length(self):
     if self.head == None:
@@ -32,7 +32,7 @@ class LinkedList:
       print('The linked list is empty.')
     else:
       for i in range(self.length()):
-        print(f'Node {i}: {current.data}')
+        print(f'{current.data[0]}: {current.data[1]}')
         current = current.next
         
   def find(self,item):
@@ -55,5 +55,25 @@ class LinkedList:
         return -1
 
   def replace(self, item):
-    item.next = None
-    self.head = item
+    next_item = item.get_next()
+    next_next_item = next_item.get_next()
+    if next_next_item == None:
+      item.next = None
+    else:
+
+      if self.find(item.data[0]) != -1:
+        current = self.head
+        second = self.head
+
+        while second.get_next() != None:
+          if second.get_next().data[0] == current.data[0]:
+            if second.get_next().get_next() != None:
+              second.next = second.get_next().get_next()
+            if second.get_next().get_next() == None:
+              second.next = None
+            if second == None:
+              break
+          else:
+            second = second.get_next()
+
+      item.next = next_item
