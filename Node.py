52cecc36d2bb1952290c463acb8713fe3dd22cb3
benchan777 <benchan777@gmail.com>
2021-02-19 16:07:56 -0800

    add get_next method

--- Node.py
+++ Node.py
@@ -3,3 +3,9 @@ class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
+
+  def get_next(self):
+    return self.next
+
+  def set_next(self, item):
+    self.next = item
